ca2daddb28939b640b1d806f8c5e0f1e57a98758
jaehoo <im.jaehoo1@gmail.com>
2022-12-11 23:04:09 +0900

    remove console.log

--- apps/admin/src/pages/test.jsx
+++ apps/admin/src/pages/test.jsx
@@ -4,8 +4,6 @@ import { useEffect } from "react";
 export default function TestPage() {
   const router = useRouter();
 
-  console.log(router);
-
   useEffect(() => {
     `https://${query.mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app&scope=mall.read_application`,
       router.replace();
