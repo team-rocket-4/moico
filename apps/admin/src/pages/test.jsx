c7df5e44f5ce6cc34d8dd3d714688ff1760cd1ce
Jaehoo-dev <54227178+Jaehoo-dev@users.noreply.github.com>
2022-12-11 23:06:09 +0900

    Redirect (#3)
    * redirect
    
    * remove console.log

--- apps/admin/src/pages/test.jsx
+++ apps/admin/src/pages/test.jsx
@@ -1,26 +1,13 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function TestPage() {
-  const { query } = useRouter();
+  const router = useRouter();
 
-  console.log(query);
+  useEffect(() => {
+    `https://${query.mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app&scope=mall.read_application`,
+      router.replace();
+  });
 
-  return (
-    <div>
-      test
-      <button
-        onClick={async () => {
-          const res = await fetch("http://localhost:4000", {
-            method: "POST",
-          });
-
-          const data = await res.json();
-
-          console.log(data);
-        }}
-      >
-        OK
-      </button>
-    </div>
-  );
+  return <div>test</div>;
 }
