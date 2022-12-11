5632044446ff2764b1b2936cdc8dcf60eac450e5
jaehoo <im.jaehoo1@gmail.com>
2022-12-11 23:46:17 +0900

    auto redirect

--- apps/admin/src/pages/test.jsx
+++ apps/admin/src/pages/test.jsx
@@ -1,22 +1,14 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function TestPage() {
   const router = useRouter();
 
-  console.log(router.query);
+  useEffect(() => {
+    router.replace(
+      `https://${router.query.mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
+    );
+  }, [router]);
 
-  return (
-    <div>
-      test
-      <button
-        onClick={() => {
-          router.replace(
-            `https://imjaehoo.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
-          );
-        }}
-      >
-        go
-      </button>
-    </div>
-  );
+  return <div>test</div>;
 }
