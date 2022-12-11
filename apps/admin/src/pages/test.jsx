4befdcaf4216013d60a3378c18b0ecfd29718a3b
jaehoo <im.jaehoo1@gmail.com>
2022-12-11 23:37:41 +0900

    test

--- apps/admin/src/pages/test.jsx
+++ apps/admin/src/pages/test.jsx
@@ -4,9 +4,11 @@ import { useEffect } from "react";
 export default function TestPage() {
   const router = useRouter();
 
+  console.log(router.query);
+
   useEffect(() => {
     router.replace(
-      `https://${router.query.mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
+      `https://imjaehoo.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
     );
   });
 
