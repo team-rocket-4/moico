98652c1631ed8485619acdf29c7ca656ef58b557
jaehoo <im.jaehoo1@gmail.com>
2022-12-11 23:34:47 +0900

    fix

--- apps/admin/src/pages/test.jsx
+++ apps/admin/src/pages/test.jsx
@@ -5,8 +5,9 @@ export default function TestPage() {
   const router = useRouter();
 
   useEffect(() => {
-    `https://${router.query.mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
-      router.replace();
+    router.replace(
+      `https://${router.query.mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
+    );
   });
 
   return <div>test</div>;
