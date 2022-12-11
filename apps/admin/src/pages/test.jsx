4e52aeac7aafd59977a5577bc39a8cc2175be5ba
jaehoo <im.jaehoo1@gmail.com>
2022-12-11 23:48:09 +0900

    guard

--- apps/admin/src/pages/test.jsx
+++ apps/admin/src/pages/test.jsx
@@ -5,8 +5,14 @@ export default function TestPage() {
   const router = useRouter();
 
   useEffect(() => {
+    const mallId = router.query.mall_id;
+
+    if (mallId == null) {
+      return;
+    }
+
     router.replace(
-      `https://${router.query.mall_id}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
+      `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
     );
   }, [router]);
 
