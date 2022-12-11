3d45f81b5b70e7be96bb13c2cab9cb3d7ffc9957
jaehoo <im.jaehoo1@gmail.com>
2022-12-11 21:05:20 +0900

    fix tsconfig

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  async postHello() {
+    const res = await fetch(
+      'https://imjaehoo.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app&scope=mall.read_application',
+    );
+
+    const data = await res.json();
+
+    console.log(data);
+
+    return data;
   }
 }
