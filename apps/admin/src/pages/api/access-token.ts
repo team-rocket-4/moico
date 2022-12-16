69907dc45d8577361ab906ad878d8ebfa12ea590
jaehoo <im.jaehoo1@gmail.com>
2022-12-16 20:30:16 +0900

    access token

---
+++ apps/admin/src/pages/api/access-token.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "GET") {
+    const response = await fetch(
+      "https://imjaehoo.cafe24api.com/api/v2/oauth/token",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            "OwndE7DgN1Nv2RsPA2euHG:OwndE7DgN1Nv2RsPA2euHG",
+          ).toString("base64")}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify({
+          grant_type: "authorization_code",
+          code: req.query.code,
+          redirect_uri: "https://moico-admin.vercel.app/really-done",
+        }),
+      },
+    );
+
+    const result = await response.json();
+
+    res.status(200).json(result);
+  }
+}
--- apps/admin/src/pages/done.tsx
+++ apps/admin/src/pages/done.tsx
@@ -1,3 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+
 export default function DonePage() {
+  const { query } = useRouter();
+
+  const { data } = useQuery(["access-token"], async () => {
+    const res = await fetch(`/api/access-token?code=${query.code}`);
+
+    console.log(res);
+
+    return res;
+  });
+
+  console.log(data);
+
   return <h1>done</h1>;
 }
