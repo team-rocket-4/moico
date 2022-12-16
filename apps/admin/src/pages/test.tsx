b65826faa95cef88b6809a7bba2791423ec56154
jaehoo <im.jaehoo1@gmail.com>
2022-12-16 19:51:44 +0900

    refactor

--- apps/admin/src/pages/test.tsx
+++ apps/admin/src/pages/test.tsx
@@ -2,19 +2,19 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function TestPage() {
-  const router = useRouter();
+  const { query, replace } = useRouter();
 
   useEffect(() => {
-    const mallId = router.query.mall_id;
+    const mallId = query.mall_id;
 
     if (mallId == null) {
       return;
     }
 
-    router.replace(
-      `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
+    replace(
+      `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
     );
-  }, [router]);
+  }, [query.mall_id, replace]);
 
   return <div>test</div>;
 }
