dc73182f120d54b42ba63de9b246134ec59c2dee
jaehoo <im.jaehoo1@gmail.com>
2022-12-16 20:12:44 +0900

    rollback

--- apps/admin/src/pages/test.tsx
+++ apps/admin/src/pages/test.tsx
@@ -12,7 +12,7 @@ export default function TestPage() {
     }
 
     replace(
-      `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done?mall-id=${mallId}&scope=mall.read_application`,
+      `https://${mallId}.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=OwndE7DgN1Nv2RsPA2euHG&state=1234&redirect_uri=https://moico-admin.vercel.app/done&scope=mall.read_application`,
     );
   }, [query.mall_id, replace]);
 
