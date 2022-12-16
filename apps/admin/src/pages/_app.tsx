5c12a36715ba1cb0fb16c55606951ccdd421e5dd
jaehoo <im.jaehoo1@gmail.com>
2022-12-16 20:06:56 +0900

    use QueryClient

--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -1,16 +1,21 @@
-// src/pages/_app.tsx
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </SessionProvider>
   );
 };
