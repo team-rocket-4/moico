c8690882c231a4da65c7b960f2d4d3e1477ea8ee
jaehoo <im.jaehoo1@gmail.com>
2022-12-17 01:09:32 +0900

    fix

--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -1,3 +1,9 @@
+import { useEffect } from "react";
+
 export default function AdminHome() {
+  useEffect(() => {
+    localStorage.getItem("@cafe24/auth-info");
+  }, []);
+
   return <div>Admin Home</div>;
 }
