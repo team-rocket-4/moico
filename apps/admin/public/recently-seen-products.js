4d8bbbc1bc848aa7fe2c2e05ece6ba8085f53eb8
jaehoo <im.jaehoo1@gmail.com>
2022-12-18 23:20:16 +0900

    update script

--- apps/admin/public/recently-seen-products.js
+++ apps/admin/public/recently-seen-products.js
@@ -1,14 +1,31 @@
 (() => {
   // Get the queue of recently-seen-products from local storage
-  const key = "recently-seen-products";
+  const key = "@moico/cafe24/recently-seen-products";
   let queue = JSON.parse(localStorage.getItem(key)) || [];
+
+  const container = document.createElement("div");
+  container.style.marginTop = "40px";
+
+  const title = document.createElement("h2");
+  title.style.marginBottom = "40px";
+  title.textContent = "최근에 본 상품";
+  container.append(title);
+
+  const imagesWrapper = document.createElement("div");
 
   // Render the images in the queue
   queue.forEach((item) => {
     const img = document.createElement("img");
     img.src = item.sImgSrc;
-    document.body.appendChild(img);
+    imagesWrapper.append(img);
   });
+
+  container.append(imagesWrapper);
+
+  const referenceNode = document.querySelector(
+    ".xans-product.xans-product-additional",
+  );
+  referenceNode.append(container);
 
   // Get current product data from session storage
   const localRecentProduct1 = JSON.parse(
