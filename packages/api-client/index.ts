78a6461f429a89cc709338f0192d60979dc2844d
jaehoo <im.jaehoo1@gmail.com>
2022-12-30 02:13:26 +0900

    remove script

--- packages/api-client/index.ts
+++ packages/api-client/index.ts
@@ -1,8 +1,9 @@
-import { get, post } from "./src/core";
+import { get, post, deleteRequest } from "./src/core";
 
 // NOTE(@Jaehoo-dev): 지금은 coreApi를 직접 사용한다.
 // 나중에 coreApi를 이용해 appApi, cafe24Api 등을 만들어 사용한다.
 export const coreApi = {
   get,
   post,
+  delete: deleteRequest,
 };
--- packages/api/src/routers/cafe24.ts
+++ packages/api/src/routers/cafe24.ts
@@ -2,6 +2,40 @@ import { coreApi } from "@moico/api-client";
 import { router, publicProcedure } from "../trpc";
 import { z } from "zod";
 
+interface AccessTokenResponse {
+  access_token: string;
+  expires_at: string;
+  refresh_token: string;
+  refresh_token_expires_at: string;
+  client_id: string;
+  mall_id: string;
+  user_id: string;
+  scopes: string[];
+  issued_at: string;
+}
+
+interface ScriptTag {
+  shop_no: number;
+  script_no: string;
+  client_id: string;
+  src: string;
+  display_location: string[];
+  exclude_path: string[];
+  skin_no: number[];
+  integrity: string;
+  created_date: string;
+  updated_date: string;
+}
+
+interface CreateScriptResponse {
+  scripttag: ScriptTag;
+}
+
+interface GetScriptsResponse {
+  scripttags: ScriptTag[];
+}
+
+// TODO(@Jaehoo-dev): 카페24 공통 라우터와 각 앱 라우터 분리
 export const cafe24Router = router({
   accessToken: publicProcedure
     .input(
@@ -11,7 +45,7 @@ export const cafe24Router = router({
       }),
     )
     .query(async ({ input: { mallId, code } }) => {
-      return coreApi.post(
+      return coreApi.post<AccessTokenResponse>(
         `https://${mallId}.cafe24api.com/api/v2/oauth/token`,
         {
           grant_type: "authorization_code",
@@ -36,7 +70,7 @@ export const cafe24Router = router({
       }),
     )
     .mutation(({ input: { mallId, accessToken } }) => {
-      return coreApi.post(
+      return coreApi.post<CreateScriptResponse>(
         `https://${mallId}.cafe24api.com/api/v2/admin/scripttags`,
         {
           request: {
@@ -44,6 +78,38 @@ export const cafe24Router = router({
             display_location: ["PRODUCT_DETAIL"],
           },
         },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      );
+    }),
+  removeScript: publicProcedure
+    .input(
+      z.object({
+        mallId: z.string(),
+        accessToken: z.string(),
+      }),
+    )
+    .mutation(async ({ input: { mallId, accessToken } }) => {
+      const { scripttags: scripts } = await coreApi.get<GetScriptsResponse>(
+        `https://${mallId}.cafe24api.com/api/v2/admin/scripttags`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      );
+      const target = scripts.find((script) => {
+        return (
+          script.src ===
+          "https://moico-admin.vercel.app/recently-seen-products.js"
+        );
+      });
+
+      return coreApi.delete<{ scripttage: { script_no: string } }>(
+        `https://${mallId}.cafe24api.com/api/v2/admin/scripttags/${target?.script_no}`,
         {
           headers: {
             Authorization: `Bearer ${accessToken}`,
