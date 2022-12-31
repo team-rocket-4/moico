7582bad7f9f528cff4c2e87e5ff6c82a24094750
jaehoo <im.jaehoo1@gmail.com>
2022-12-31 21:24:30 +0900

    create app server

--- packages/api-client/src/core/get.ts
+++ packages/api-client/src/core/get.ts
@@ -2,10 +2,7 @@ export async function get<ResponseType>(
   url: string,
   options: Omit<RequestInit, "method" | "body"> = {},
 ) {
-  const res = await fetch(url, {
-    credentials: "include",
-    ...options,
-  });
+  const res = await fetch(url, options);
 
   if (!res.ok) {
     throw new Error(await res.text());
