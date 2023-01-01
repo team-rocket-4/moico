1f8ec7a8df637094eb3a98120ea3462ab1ac7474
jaehoo <im.jaehoo1@gmail.com>
2023-01-01 22:40:33 +0900

    axios

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { cafe24ClientId } from './constants/cafe24-client-id';
-import got from 'got';
+import axios from 'axios';
 
 @Controller()
 export class AppController {
@@ -17,7 +17,7 @@ export class AppController {
     @Param('mallId') mallId: string,
     @Param('productId') productId: number,
   ) {
-    return got.get(
+    return axios.get(
       `https://${mallId}.cafe24api.com/api/v2/productsdetail/${productId}`,
       {
         headers: {
